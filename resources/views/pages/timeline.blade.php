fd21273688aeda9671d85a7af679ca8ae0351fb2
German Ojeda <gojeda@megatlon.com.ar>
2019-08-30 15:38:48 -0300

    Tamaño imagen timeline

--- resources/views/pages/timeline.blade.php
+++ resources/views/pages/timeline.blade.php
@@ -6,13 +6,16 @@
       
       @foreach ($imagenes as $imagen)
        
-        {{ $imagen->users->name }}
+        
 
         <div class="publicaciones_timeline">
             <img src="{{ route('user.posts', ['user' => $imagen->users->nick, 'filename' => $imagen->image_path]) }}" />
+            <div class="details">
+              {{ $imagen->users->name }}
+            </div>
             <div class="parte_negra">
               <ul>
-                <li class="comentar"><i class="material-icons">comment</i></li>
+                <li class="comentar"><a href=""><i class="material-icons">comment</i></a></li>
                 <li class="likear"><i class="material-icons">thumb_up</i></li>
                 <li class="compartir"><i class="material-icons">share</i></li>
               </ul>
